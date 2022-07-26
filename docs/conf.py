a82c8151c33aad7822ceee6480c384b55f157f97
MarineLife <ghdic77@gmail.com>
2022-07-26 23:56:32 +0900

    dd

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'marinelife'
+project = 'sphinx'
 copyright = '2022, ghdic'
 author = 'ghdic'
 release = '1.0'
