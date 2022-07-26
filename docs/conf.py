ede8a3669a481306fed0c1deaca64fe0d6e6da87
MarineLife <ghdic77@gmail.com>
2022-07-26 23:39:56 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,12 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme']
+extensions = ['sphinx_rtd_theme', 'myst_parser']
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
